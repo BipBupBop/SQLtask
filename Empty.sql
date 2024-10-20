b2cccf07afe0b599c86b200b57ee78332850db90
Krupin Aleksandr <76613067+BipBupBop@users.noreply.github.com>
2024-10-20 20:14:41 +0300

    Update Empty.sql

--- Empty.sql
+++ Empty.sql
@@ -1,1 +1,15 @@
+-- 2. Получить список карточек с указанием имени владельца, баланса и названия банка
+GO
+USE BankDb;
+GO
 
+SELECT c.number AS cardNumber, 
+cl.fullName AS ownerName, 
+a.balance, 
+b.bankName
+FROM Cards c
+JOIN Accounts a ON c.accountId = a.id
+JOIN Clients cl ON a.clientId = cl.id
+JOIN Banks b ON a.bankId = b.id;
+
+GO
